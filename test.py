52cb79125936c9f0b88b61870ce7acc76e49164f
Ashim Paudel <70967085+Ashim-Paudel@users.noreply.github.com>
2021-02-11 16:42:39 +0545

    unnecessary file deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-high_score = open("test_high.txt", "a+")
-
-
-high_score.seek(0)
-
-list_of_scores = [int(scores) for scores in high_score.readlines()]
-
-print(list_of_scores)
-print(f"High score is {max(list_of_scores)}")
